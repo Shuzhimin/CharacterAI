9b683d03a893f05eefbfa8db2ab4b78ae6fff44f
zhangzhong <im.zhong@outlook.com>
2024-02-06 23:42:47 +0800

    remove deprecated dependencies and modules

--- app_refactor/dependencies.py
+++ app_refactor/dependencies.py
@@ -1,23 +1,7 @@
 # 2024/2/6
 # zhangzhong
 
-from pymongo import MongoClient
-from app_refactor.models import Character
 from app_refactor.database.proxy import DatabaseProxy
-
-client = MongoClient("mongodb://localhost:27017/", port=27017)
-db = client["CharacterAI"]
-collection = db["character_info"]
-
-
-async def query_character_info_all(bot_name: str):
-    info_list = collection.find_one({"bot_name": bot_name})
-    # 解包字典, 生成Character对象
-    if info_list:
-        character_info = Character(**info_list)
-        return character_info
-    else:
-        return None
 
 
 def database_proxy() -> DatabaseProxy:
