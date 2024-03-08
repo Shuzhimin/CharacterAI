3c78ab4c9932e9a3dbd60226a68ab9c02d7ea591
li lin xuan <you@example.com>
2024-03-08 11:43:40 +0800

    2

--- app/routers/character.py
+++ app/routers/character.py
@@ -9,28 +9,28 @@ import app.common.glm as glm
 router = APIRouter()
 
 
-# 获取机器人名称列表
-@router.get(path="/names/query")
-async def get_bot_names(
-    db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)]
-) -> dict[str, list[str]]:
-    error, characters = db.get_all_characters()
-    # if not error.ok() or characters is None:
-    #     # TODO: do some logging
-    #     pass
-    return {"bot_names": [character.bot_name for character in characters]}
+# # 获取机器人名称列表
+# @router.get(path="/names/query")
+# async def get_bot_names(
+#     db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)]
+# ) -> dict[str, list[str]]:
+#     error, characters = db.get_all_characters()
+#     # if not error.ok() or characters is None:
+#     #     # TODO: do some logging
+#     #     pass
+#     return {"bot_names": [character.bot_name for character in characters]}
 
 
-# 根据机器人名称查询机器人信息
-@router.get(path="/character/query")
-async def query_character_info(
-    bot_name: str,
-    db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)],
-) -> dict[str, str]:
-    error, character = db.get_character_by_botname(botname=bot_name)
-    if not error.ok() or character is None:
-        raise HTTPException(status_code=404, detail=f"'{bot_name}' not found")
-    return character.dump_character_info_without_chat_history()
+# # 根据机器人名称查询机器人信息
+# @router.get(path="/character/query")
+# async def query_character_info(
+#     bot_name: str,
+#     db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)],
+# ) -> dict[str, str]:
+#     error, character = db.get_character_by_botname(botname=bot_name)
+#     if not error.ok() or character is None:
+#         raise HTTPException(status_code=404, detail=f"'{bot_name}' not found")
+#     return character.dump_character_info_without_chat_history()
 
 
 # 删除机器人 接口1.2 删除角色 /character/delete
@@ -92,28 +92,28 @@ async def create_character(
     return {"code": err.code, "message": err.message, "data": []}
 
 
-# 聊天
-@router.post(path="/character/chat")
-async def chat(
-    bot_name: str,
-    content: str,
-    db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)],
-) -> dict[str, Any]:
-    error, character = db.get_character_by_botname(botname=bot_name)
-    if not error.ok() or character is None:
-        return {
-            "success": "fail",
-            "content": "character not found",
-            "chat_history": [],
-        }
-
-    character.chat_history.append(ChatRecord(who="user", message=content))
-    response = glm.invoke_character_glm_api(character=character)
-    content = glm.get_content_from_response(response=response)
-    character.chat_history.append(ChatRecord(who="assistant", message=content))
-    db.update_character(character=character)
-    return {
-        "success": response["success"],
-        "content": response["data"]["choices"][0]["content"],
-        "chat_history": character.dump_chat_history(),
-    }
+# # 聊天
+# @router.post(path="/character/chat")
+# async def chat(
+#     bot_name: str,
+#     content: str,
+#     db: Annotated[DatabaseProxy, Depends(dependency=database_proxy)],
+# ) -> dict[str, Any]:
+#     error, character = db.get_character_by_botname(botname=bot_name)
+#     if not error.ok() or character is None:
+#         return {
+#             "success": "fail",
+#             "content": "character not found",
+#             "chat_history": [],
+#         }
+#
+#     character.chat_history.append(ChatRecord(who="user", message=content))
+#     response = glm.invoke_character_glm_api(character=character)
+#     content = glm.get_content_from_response(response=response)
+#     character.chat_history.append(ChatRecord(who="assistant", message=content))
+#     db.update_character(character=character)
+#     return {
+#         "success": response["success"],
+#         "content": response["data"]["choices"][0]["content"],
+#         "chat_history": character.dump_chat_history(),
+#     }
