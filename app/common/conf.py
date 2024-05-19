e6ced1a3eff049b22a4dc89c683458f35045bd3c
Shuzhimin <1193846782@qq.com>
2024-05-19 14:27:38 +0800

    clean mongo

--- app/common/conf.py
+++ app/common/conf.py
@@ -28,16 +28,6 @@ class Collections(BaseModel):
     character: str = Field(description="角色信息集合")
 
 
-class MongoConf(BaseModel):
-    host: str = Field(description="主机")
-    port: int = Field(description="端口")
-    username: str = Field(description="用户名")
-    password: str = Field(description="密码")
-    authSource: str = Field(description="认证数据库")
-    database: str = Field(description="数据库")
-    collections: Collections = Field(description="集合")
-
-
 class FastAPIConf(BaseModel):
     host: str = Field(description="主机")
     port: int = Field(description="端口")
@@ -71,7 +61,6 @@ class Conf:
 
     def __init__(self, conf: dict[str, Any]):
         self.check_conf(conf=conf)
-        self.mongo = MongoConf(**conf["mongo"])
         self.zhipuai = ZhipuAIConf(**conf["zhipuai"])
         self.fastapi = FastAPIConf(**conf["fastapi"])
         self.postgres = PostgresConf(**conf["postgres"])
@@ -81,22 +70,10 @@ class Conf:
         self.llm = LLMConf(**conf["llm"])
 
     def check_conf(self, conf: dict[str, Any]) -> None:
-        keys: list[str] = ["mongo", "fastapi", "zhipuai"]
+        keys: list[str] = ["fastapi", "zhipuai", "minio", "admin", "qdrant", "llm"]
         for key in keys:
             if key not in conf:
                 raise Exception(f"配置文件中缺少{key}字段")
-
-    def get_mongo_setting(self) -> dict:
-        mongo_setting = self.mongo.model_dump()
-        mongo_setting.pop("database")
-        mongo_setting.pop("collections")
-        return mongo_setting
-
-    def get_mongo_database(self) -> str:
-        return self.mongo.database
-
-    def get_mongo_character_collname(self) -> str:
-        return self.mongo.collections.character
 
     def get_fastapi_host(self) -> str:
         return self.fastapi.host
